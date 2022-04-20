d0037479bb7713ae2b5e7064289ebeb3194fac37
Tyler <tylerclancy030@gmail.com>
2022-04-20 08:17:05 -0500

    Escape

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ import { Button } from '@nextui-org/react';
 export default function Custom404() {
   return (
     <section className='juicer'>
-      <h1>404 - That page doesn't seem to exist...</h1>
+      <h1>404 - That page does not seem to exist...</h1>
       <iframe src="https://giphy.com/embed/ZNnQvIYzIBmZAbrBR7" width="480" height="270" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/boomerangtoons-boomerang-garfield-and-friends-ZNnQvIYzIBmZAbrBR7">via GIPHY</a></p>
       <Link href='/'>
         <Button>Take me home</Button>
